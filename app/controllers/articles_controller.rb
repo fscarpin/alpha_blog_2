3910b5a22cb4216351b311c1686d672f042cbeee
fscarpin <fernando.scarpin@gmail.com>
2016-05-11 19:31:46 -0300

    Added error message handling to view new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,20 @@ class ArticlesController < ApplicationController
   def create
     #   render plain: params[:article].inspect 
     @article = Article.new(article_params)
-        
     if @article.save
-      # The hash flash will only store the message for next request. After that it'll be cleared
       flash[:notice] = "Article has been saved successfully"
       redirect_to article_path(@article)
     else
-            render :new
+      render :new
     end
+    # if @article.save
+    #   # The hash flash will only store the message for next request. After that it'll be cleared
+    #   flash[:notice] = "Article has been saved successfully"
+    #   # redirect_to article_path(@article)
+    #   render plain: "test"
+    # else
+    #         render :new
+    # end
   end
     
   private
