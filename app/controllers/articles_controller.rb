4178ca57e5cbce2424f1c87b06aa46aaaed7a45e
fscarpin <fernando.scarpin@gmail.com>
2016-05-12 11:09:34 -0300

    Edit has been added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,25 @@
 class ArticlesController < ApplicationController
-  
+
   def new
     @article = Article.new
   end
-    
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article has been edited successfully"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
   def create
-    #   render plain: params[:article].inspect 
+    #   render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article has been saved successfully"
@@ -26,7 +40,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-    
+
   private
     def article_params
       params.require(:article).permit(:title, :description)
