f46eea81d35fb6a4cd8efec68544b8d63945c0b8
fscarpin <fernando.scarpin@gmail.com>
2016-05-16 15:56:14 -0300

    Increased the number of characters permited for article titles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ActiveRecord::Base
   # hash = {presence: true}
   # validates :title, hash
 
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :title, presence: true, length: {minimum: 3, maximum: 80}
   validates :description, presence: true, length: {minimum: 10, maximum: 3000}
 end
