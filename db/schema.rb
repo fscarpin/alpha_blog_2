b3abdb8a3d907421672f211ce02041303ac11150
fscarpin <fernando.scarpin@gmail.com>
2016-05-16 17:49:26 -0300

    Renamed column name to username in users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516201655) do
+ActiveRecord::Schema.define(version: 20160516204710) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160516201655) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
