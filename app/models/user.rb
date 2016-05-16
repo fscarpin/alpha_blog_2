baad3593418b72413f5202015ca3e01f8b546dd8
fscarpin <fernando.scarpin@gmail.com>
2016-05-16 18:37:17 -0300

    Added user validations for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
-  # validates :name, presence: true, uniqueness: true
-  # validates :email, presence: true, uniqueness: true
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 20 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 end
