d04c4aee16ebd10b94bd0e2d12597db39f888e75
fscarpin <fernando.scarpin@gmail.com>
2016-05-19 18:16:13 -0300

    Added the login and logout feature using the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,28 @@ class SessionsController < ApplicationController
   end
 
   def create
-    debugger
+    # Find the user
+    user = User.find_by(email: params[:session][:email])
+    # Get the password from the parameters
+    password = params[:session][:password]
+    # Try to authenticate the user
+    if user && user.authenticate(password)
+      flash[:success] = "Welcome back #{user.username}"
+      # Store the user id in the session
+      session[:user_id] = user.id
+      # Redirect the user to its page
+      redirect_to user_path(user)
+    else
+      # Since we are using render, there won't be another request, so we need to use flash.now
+      flash.now[:danger] = "Wrong email or password. Please try again."
+      render :new
+    end
   end
 
   def destroy
-
+    # Remove the user_id from the session, and show a message
+    session[:user_id] = nil
+    flash[:success] = "You've been successfully logged out"
+    redirect_to root_path
   end
 end
