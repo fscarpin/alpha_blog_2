cede5dfd00fd3ee1f3c0e3bd627c231f34b8769b
fscarpin <fernando.scarpin@gmail.com>
2016-05-23 16:50:46 -0300

    Bug fix: after a new user was created, the session was not being set

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Alpha Blog, #{@user.username}"
+      session[:user_id] = @user.id
       redirect_to articles_path
     else
       render :new
