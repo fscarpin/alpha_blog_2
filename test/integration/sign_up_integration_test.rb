3ca303a9df9e5ff7c9b9b08db1506c715c9576a1
fscarpin <fernando.scarpin@gmail.com>
2016-05-28 15:56:28 -0300

    Added new article creation integration test

--- test/integration/sign_up_integration_test.rb
+++ test/integration/sign_up_integration_test.rb
@@ -2,22 +2,18 @@ require "test_helper"
 
 class SignUpIntegrationTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @user = User.create(username: "foo", password: "temp123", email: "foo@example.com", admin: true)
-  end
-
-  test "get new category form and create category" do
-    login_as @user
-    # New Article
-    get new_article_path
-    assert_template "articles/new"
-    # Create a new article
-    assert_difference "Article.count", 1 do
-      post_via_redirect articles_path, article: {title: "This is a new title", description: "This is a new description"}
+  test "sign up user" do
+    # Go to the sign up page
+    get signup_path
+    assert_template "users/new"
+    #Signup the new user
+    assert_difference "User.count", 1 do
+      post_via_redirect users_path, user: {username: "foo", email: "foo@example.com", password: "password",
+                                           confirm_your_password: "password"}
     end
-    # Make sure the right page is showing up
-    assert_template "articles/show"
-    assert_match "This is a new title", response.body
+    # Welcome user
+    assert_template "users/show"
+    assert_match "Welcome to foo's page", response.body
   end
 
 end
